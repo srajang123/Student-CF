aed8113e3857d4a1a89a98257ed6aac6def5969d
SRAJAN <srajan.oel@gmail.com>
2020-06-17 13:13:07 +0530

    Migrated App.js to React Hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import './App.css';
 import {Switch,Link,Route,BrowserRouter as Router} from 'react-router-dom';
 import axios from 'axios';
@@ -7,83 +7,69 @@ import Home from './Components/Home';
 import Add from './Components/Add';
 import View from './Components/View';
 import Stud from './Components/Stud';
-export default class App extends React.Component
-{
-  constructor(props)
-  {
-    super(props)
-    {
-      this.state={
-        studs:[],
-        new:{
-          name:'',
-          age:'',
-          gender:'',
-          cf:''
-        }
-      }
-    }
+
+function App() {
+  const [studs, setStud] = useState([]);
+  const [New, setNew] = useState({
+    name: '',
+    age: '',
+    gender: '',
+    cf: ''
+  });
+  useEffect(() => {
+    axios.get('http://localhost:5000/view')
+      .then(res => setStud(res.data))
+      .catch(err => console.log(err))
+  });
+  const SetNew = (dat) => {
+    setNew(dat);
   }
-  SetNew=(dat)=>{
-    this.setState({
-      new:dat
-    })
-  }
-  editName=(e)=>{
-    let data=this.state.new;
+  const editName = (e) => {
+    let data = this.state.new;
     data.name = e.target.value;
-    this.SetNew(data);
+    SetNew(data);
   }
-  editAge = (e) => {
+  const editAge = (e) => {
     let data = this.state.new;
     data.age = e.target.value;
-    this.SetNew(data);
+    SetNew(data);
   }
-  editGender = (e) => {
+  const editGender = (e) => {
     let data = this.state.new;
     data.gender = e.target.value;
-    this.SetNew(data);
+    SetNew(data);
   }
-  editCF = (e) => {
+  const editCF = (e) => {
     let data = this.state.new;
     data.cf = e.target.value;
-    this.SetNew(data);
+    SetNew(data);
   }
-  RESET=(e)=>{
-    let data={
-      name:'',
-      age:'',
-      gender:'',
-      cf:''
+  const RESET = (e) => {
+    let data = {
+      name: '',
+      age: '',
+      gender: '',
+      cf: ''
     }
-    this.SetNew(data);
+    SetNew(data);
   }
-  SUBMIT=(e)=>{
-    axios.post('http://localhost:5000/add',this.state.new)
-    .then(res=>console.log(res))
-    .catch(err=>console.log(err));
-    this.RESET();
+  const SUBMIT = (e) => {
+    axios.post('http://localhost:5000/add', this.state.new)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+    RESET();
   }
-  componentDidMount=()=>{
-    axios.get('http://localhost:5000/view')
-    .then(res=>this.setState({
-      studs:res.data
-    }))
-    .catch(err=>console.log(err))
-    console.log(this.state.studs);
-  }
-  render=()=>{
-    return(
+  return (
     <div class="test-app">
-          <Router>
-            <Switch>
-              <Route path='/add'><Add Name={this.editName} Age={this.editAge} Gender={this.editGender} CF={this.editCF} RESET={this.RESET} SUBMIT={this.SUBMIT} new={this.state.new}/></Route>
-              <Route path='/view'><View data={this.state.studs}/></Route>
-              <Route path='/stud/'><Stud data={this.state.studs}/></Route>
-              <Route path='/'><Home /></Route>
-            </Switch>
-          </Router>
+      <Router>
+        <Switch>
+          <Route path='/add'><Add Name={editName} Age={editAge} Gender={editGender} CF={editCF} RESET={RESET} SUBMIT={SUBMIT} new={New} /></Route>
+          <Route path='/view'><View data={studs} /></Route>
+          <Route path='/stud/'><Stud data={studs} /></Route>
+          <Route path='/'><Home /></Route>
+        </Switch>
+      </Router>
     </div>
-    )
-  }
+  )
 }
+export default App;
