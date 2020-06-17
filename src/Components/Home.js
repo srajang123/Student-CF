4dee7133b277b4355cc22b4a889d38c2564d2a3c
SRAJAN <srajan.oel@gmail.com>
2020-06-17 13:14:17 +0530

    Migrated /Components/Home.js to React Hooks

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,7 @@
-import React,{Component} from 'react';
+import React,{useEffect} from 'react';
 
-export default class Home extends Component
-{
-    render=()=>{
-        return(
-        <h1>Students Data App</h1>
-        )
-    }
+function App(){
+    return(<h1>Students Data App</h1>)
 }
+
+export default App;
