d74afa8e39d6704eaa91ebfdada5e8d392b14534
SRAJAN <srajan.oel@gmail.com>
2020-06-17 13:20:49 +0530

    Migrated /Components/Add.js to React Hooks

--- src/App.js
+++ src/App.js
@@ -25,22 +25,22 @@ function App() {
     setNew(dat);
   }
   const editName = (e) => {
-    let data = this.state.new;
+    let data = New;
     data.name = e.target.value;
     SetNew(data);
   }
   const editAge = (e) => {
-    let data = this.state.new;
+    let data = New;
     data.age = e.target.value;
     SetNew(data);
   }
   const editGender = (e) => {
-    let data = this.state.new;
+    let data = New;
     data.gender = e.target.value;
     SetNew(data);
   }
   const editCF = (e) => {
-    let data = this.state.new;
+    let data = New;
     data.cf = e.target.value;
     SetNew(data);
   }
@@ -54,7 +54,7 @@ function App() {
     SetNew(data);
   }
   const SUBMIT = (e) => {
-    axios.post('http://localhost:5000/add', this.state.new)
+    axios.post('http://localhost:5000/add', New)
       .then(res => console.log(res))
       .catch(err => console.log(err));
     RESET();
@@ -63,7 +63,7 @@ function App() {
     <div class="test-app">
       <Router>
         <Switch>
-          <Route path='/add'><Add Name={editName} Age={editAge} Gender={editGender} CF={editCF} RESET={RESET} SUBMIT={SUBMIT} new={New} /></Route>
+          <Route path='/add'><Add Name={editName} Age={editAge} Gender={editGender} CF={editCF} RESET={RESET} SUBMIT={SUBMIT} New={New} /></Route>
           <Route path='/view'><View data={studs} /></Route>
           <Route path='/stud/'><Stud data={studs} /></Route>
           <Route path='/'><Home /></Route>
